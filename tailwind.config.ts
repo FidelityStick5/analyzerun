4c320f3d8dc991bfbdda8da921146ef5eaff67f2
FidelityStick5 <79571925+FidelityStick5@users.noreply.github.com>
2024-07-30 10:59:11 +0200

    Add Dracula color palette to TailwindCSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        dracula: {
+          foreground: "#F8F8F2",
+          background: "#282A36",
+          selection: "#44475A",
+          comment: "#6272A4",
+          red: "#FF5555",
+          orange: "#FFB86C",
+          yellow: "#F1FA8C",
+          green: "#50FA7B",
+          purple: "#BD93F9",
+          cyan: "#8BE9FD",
+          pink: "#FF79C6",
+        },
       },
     },
   },
