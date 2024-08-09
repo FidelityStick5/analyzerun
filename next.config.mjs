71bf36d1974afab7ed9e71bb077167679b1e17ac
FidelityStick5 <79571925+FidelityStick5@users.noreply.github.com>
2024-08-09 15:56:29 +0200

    Fix SVGR setup

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,21 @@ const nextConfig = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
+      loader: "@svgr/webpack",
+      options: {
+        svgoConfig: {
+          plugins: [
+            {
+              name: "preset-default",
+              params: {
+                overrides: {
+                  removeViewBox: false,
+                },
+              },
+            },
+          ],
+        },
+      },
     });
 
     return config;
