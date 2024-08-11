5970b48a0b85d2db49a54e646bc660b3e27f0ac1
FidelityStick5 <79571925+FidelityStick5@users.noreply.github.com>
2024-08-11 12:25:15 +0200

    Add dashboard endpoint protection

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,16 +1,21 @@
 import type { Metadata } from "next";
 import Navbar from "./Navbar";
+import getUser from "@/utils/getUser";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Analyzerun",
   description: "Improve your running performance easily with Analyzerun",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { isAuthenticated } = await getUser();
+  if (!isAuthenticated) return redirect("/unauthenticated");
+
   return (
     <main className="grid h-screen w-screen grid-cols-1 grid-rows-[1fr,6rem] bg-dracula-selection text-dracula-foreground md:grid-cols-[16rem,1fr] md:grid-rows-1 md:gap-4 md:p-4">
       <Navbar />
