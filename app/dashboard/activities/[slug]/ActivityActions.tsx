69cf2c60c883fc39761981cd6508d0842d5c879b
FidelityStick5 <79571925+FidelityStick5@users.noreply.github.com>
2024-10-14 17:10:11 +0200

    Add button and endpoint to delete activity
    - Improved layout for mobile phones

---
+++ app/dashboard/activities/[slug]/ActivityActions.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useContext } from "react";
+import DeleteIcon from "@/icons/delete.svg";
+import { ActivitiesContext } from "@/providers/ActivitiesProvider";
+import { ActivitiesEndpoint } from "@/types/endpoints";
+import { Activities } from "@/types/globals";
+
+export default function ActivityActions({ id }: { id: string }) {
+  const { setActivities } = useContext(ActivitiesContext);
+  const router = useRouter();
+
+  const deleteActivity = async () => {
+    const response = await fetch("/api/activities", {
+      method: "DELETE",
+      body: JSON.stringify({ id }),
+    });
+    if (!response.ok) throw response;
+    const result: ActivitiesEndpoint.DeleteResponse = await response.json();
+    if (!result.deletedCount || result.deletedCount === 0)
+      return result.message;
+
+    const data: Activities = JSON.parse(
+      localStorage.getItem("activities") || "",
+    );
+
+    data.activities = data.activities.filter(
+      (activity) => activity._id.toString() !== id,
+    );
+
+    localStorage.setItem("activities", JSON.stringify(data));
+    setActivities(data.activities);
+
+    router.replace("/dashboard/activities");
+  };
+
+  return (
+    <button
+      onClick={deleteActivity}
+      className="flex items-center justify-between gap-4 rounded bg-primary p-4 text-background transition-colors hover:border-accent hover:bg-accent"
+    >
+      <span>Delete activity</span>
+      <span className="max-sm:hidden">
+        <DeleteIcon className="fill-secondary" />
+      </span>
+    </button>
+  );
+}
--- app/dashboard/activities/[slug]/page.tsx
+++ app/dashboard/activities/[slug]/page.tsx
@@ -1,142 +1,15 @@
-"use client";
-
-import { useContext, useEffect, useState } from "react";
-import GridTile from "@/components/GridTile";
-import CaloriesIcon from "@/icons/calories.svg";
-import DateIcon from "@/icons/time.svg";
-import DistanceIcon from "@/icons/distance.svg";
-import ElevationIcon from "@/icons/elevation.svg";
-import HeartRateIcon from "@/icons/heart_rate.svg";
-import Vo2MaxIcon from "@/icons/vo2max.svg";
-import TimeIcon from "@/icons/timer.svg";
-import TitleIcon from "@/icons/tag.svg";
-import SpeedIcon from "@/icons/speed.svg";
-import { ActivitiesContext } from "@/providers/ActivitiesProvider";
-import { estimateVO2Max } from "@/utils/vo2maxUtils";
-import { Activity } from "@/types/globals";
-
-function ErrorMessage({ text }: { text: string }) {
-  return (
-    <div className="flex items-center justify-center bg-secondary20 p-4 md:rounded">
-      {text}
-    </div>
-  );
-}
+import ActivityActions from "./ActivityActions";
+import ActivityGrid from "./ActivityGrid";
 
 export default function ActivityPage({
   params: { slug },
 }: {
   params: { slug: string };
 }) {
-  const { activities, isActivitiesContextLoading } =
-    useContext(ActivitiesContext);
-  const [activity, setActivity] = useState<Activity>();
-
-  useEffect(() => {
-    if (!activities) return;
-
-    const foundActivity: Activity | undefined =
-      activities.find(({ _id }) => _id.toString() === slug) ?? undefined;
-
-    setActivity(foundActivity);
-  }, [activities]);
-
-  if (isActivitiesContextLoading)
-    return <ErrorMessage text="Loading activity" />;
-  if (!activity) return <ErrorMessage text="No activity found" />;
-
   return (
-    <div className="grid grid-cols-1 gap-4 overflow-auto bg-secondary20 p-4 md:rounded xl:grid-cols-4 xl:grid-rows-6">
-      <GridTile
-        title="Activity name"
-        data={activity.title}
-        Icon={TitleIcon}
-        iconColor="#e5e7eb"
-        gridSpan="xl:col-span-2 xl:row-span-2"
-      />
-      <GridTile
-        title="Activity type"
-        data={activity.activity_type}
-        Icon={TitleIcon}
-        iconColor="#3b82f6"
-        gridSpan="xl:row-span-2"
-      />
-      <GridTile
-        title="Activity date"
-        data={[
-          new Date(activity.date).toLocaleTimeString(),
-          new Date(activity.date).toLocaleDateString(),
-        ]}
-        Icon={DateIcon}
-        iconColor="#818cf8"
-        gridSpan="xl:row-span-2"
-      />
-
-      <GridTile
-        title="Activity distance"
-        data={`${activity.distance} km`}
-        Icon={DistanceIcon}
-        iconColor="#4ade80"
-        gridSpan="xl:row-span-2"
-      />
-      <GridTile
-        title="Activity time"
-        data={activity.time}
-        Icon={TimeIcon}
-        iconColor="#2dd4bf"
-        gridSpan="xl:row-span-2"
-      />
-      <GridTile
-        title="Average pace"
-        data={activity.average_pace}
-        Icon={SpeedIcon}
-        iconColor="#fbbf24"
-      />
-      <GridTile
-        title="Average heart rate"
-        data={activity.average_heart_rate}
-        Icon={HeartRateIcon}
-        iconColor="#ef4444"
-      />
-      <GridTile
-        title="Best pace"
-        data={activity.best_pace}
-        Icon={SpeedIcon}
-        iconColor="#fbbf24"
-      />
-      <GridTile
-        title="Max heart rate"
-        data={activity.max_heart_rate}
-        Icon={HeartRateIcon}
-        iconColor="#ef4444"
-      />
-
-      <GridTile
-        title="Minimum elevation"
-        data={activity.min_elevation}
-        Icon={ElevationIcon}
-        iconColor="#60a5fa"
-      />
-      <GridTile
-        title="Estimated VO2Max"
-        data={estimateVO2Max(activity.distance, activity.time).toFixed(2)}
-        Icon={Vo2MaxIcon}
-        iconColor="#a78bfa"
-        gridSpan="xl:col-span-2 xl:row-span-2"
-      />
-      <GridTile
-        title="Burned calories"
-        data={activity.calories + " calories"}
-        Icon={CaloriesIcon}
-        iconColor="#f87171"
-        gridSpan="xl:row-span-2"
-      />
-      <GridTile
-        title="Maximum elevation"
-        data={activity.max_elevation}
-        Icon={ElevationIcon}
-        iconColor="#60a5fa"
-      />
+    <div className="grid grid-cols-1 grid-rows-[1fr,4rem] gap-4 p-4">
+      <ActivityGrid id={slug} />
+      <ActivityActions id={slug} />
     </div>
   );
 }
